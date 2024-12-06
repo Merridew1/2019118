2ced5a14173afe4f4b6db01acf48e111158418b4
Merridew1 <jrmullettii@outlook.com>
2024-12-06 15:06:12 -0600

    got rid of warning in intake real

--- src/main/java/frc/robot/subsystems/ballMechanism/ballMechanismReal.java
+++ src/main/java/frc/robot/subsystems/ballMechanism/ballMechanismReal.java
@@ -1,12 +1,12 @@
 package frc.robot.subsystems.ballMechanism;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.google.flatbuffers.Constants;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class ballMechanismReal implements ballMechanismIO {
-    CANSparkMax neo = new CANSparkMax(frc.robot.Constants.Motors.Intake.INTAKE_MOTOR_ID, MotorType.kBrushless);
+    CANSparkMax neo =
+        new CANSparkMax(frc.robot.Constants.Motors.Intake.INTAKE_MOTOR_ID, MotorType.kBrushless);
     DigitalInput intakeBeamBrake = new DigitalInput(0);
     DigitalInput outtakeBeamBrake = new DigitalInput(1);
 
