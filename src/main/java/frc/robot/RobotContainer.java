1567df047986067b703a852c83f28c7dc89cf282
Merridew1 <jrmullettii@outlook.com>
2024-12-06 20:11:46 -0600

    LEDs fully complete

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,12 +5,15 @@ import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Robot.RobotRunType;
+import frc.robot.subsystems.LEDs;
 import frc.robot.subsystems.ballMechanism.ballMechanism;
 import frc.robot.subsystems.ballMechanism.ballMechanismIO;
 import frc.robot.subsystems.ballMechanism.ballMechanismReal;
@@ -37,6 +40,11 @@ public class RobotContainer {
     private Drivetrain drivetrain;
     private Hatch hatch;
     private ballMechanism intake;
+    private LEDs leds;
+
+    /* Trigger events */
+    Trigger ballAtIntake = new Trigger(() -> intake.getIntakeBeamBreakStatus());
+    Trigger ballAtOuttake = new Trigger(() -> intake.getOuttakeBeamBrakeStatus());
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -90,6 +98,8 @@ public class RobotContainer {
      * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
     private void configureButtonBindings() {
+        ballAtIntake.onTrue(leds.flashCommand(Color.kPink, Color.kBrown));
+        ballAtOuttake.onTrue(leds.flashCommand(Color.kBlack, Color.kRed));
         drivetrain.setDefaultCommand(drivetrain.driveCommand(driver));
         driver.a().whileTrue(hatch.setHatchUp().andThen(Commands.startEnd(() -> {
             driver.getHID().setRumble(RumbleType.kBothRumble, 1);
